10887a786ef35716d8419d13e60880fb83ef2a11
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2010-12-16 21:00:24 -0700

    Removed tree methods from IOEngine. IOEngine now has only parse() method. Created new class for building trees.

--- io.py
+++ io.py
@@ -1,5 +1,7 @@
 """Classes and functions to handle input and output streams"""
 
+import re
+
 class IOEngine(object):
     """Main class to handle input and output"""
     def __init__(self, inputDict, outputDict):
@@ -11,14 +13,6 @@ class IOEngine(object):
         tokenList = self.io.split(string)
         tree = buildTree(tokenList)
         return tree
-
-    def printTree(self, tree):
-        """Construct a string based on the expression tree"""
-        return False
-
-    def buildTree(self, tokenList):
-        """Construct a tree based on the string token list"""
-        return False
 
 
 class Classifier(object):
@@ -67,61 +61,77 @@ class IOstream(object):
         """inputDict is a dictionary containing regular expression strings
 and their associated classes to instantiate.
 
-inputDict = {regex0: class0,
-             regex1: class1,
-             regex2: class3}
+        inputDict = {regex0: class0,
+                     regex1: class1,
+                     regex2: class3}
 
-outputDict is a dictionary containing classes and their associated string tokens
-representation, where the keyword 'val' is replaced by the objects value or
-name.
+        outputDict is a dictionary containing classes and their associated string tokens
+        representation, where the keyword 'val' is replaced by the objects value or
+        name.
 
-Example:
-    If an objects has a string representation of <<(some value)>> and it's name
-    is A, the string in the output dictionary should be written as <<val>>, so
-    that the object is printed as <<A>>.
+        Example:
+            If an objects has a string representation of <<(some value)>> and it's name
+            is A, the string in the output dictionary should be written as <<val>>, so
+            that the object is printed as <<A>>.
 
-outputDict = {class0: string0,
-              class1: string1,
-              class2: string2}
-"""
+        outputDict = {class0: string0,
+                      class1: string1,
+                      class2: string2}
+        """
         self.inputDict = inputDict
         self.outputDict = outputDict
 
-def split(self, string):
-    """Split string into valid string tokens"""
-    # Pipe Cleaner - fix problems arising from pipes in inner products
-    # This is only a problem with qm.py, need to move this to a preprocess
-    # method
-    innerProdMatches = re.finditer(r'<[a-zA-Z][a-zA-Z0-9]*[|][a-zA-Z][a-zA-Z0-9]*>', s)
-    for InnerProd in innerProdMatches:
-        subStr = InnerProd.group(0)
-        # double the pipe within the inner product
-        s = s.replace(subStr, subStr.replace('|', '||'))
-        
-    tokenList = []
-    for regex in self.inputDict:
-        tokenMatches = re.finditer(regex, s) # find all matches in string
-        for eachMatch in tokenMatches:
-            loc = eachMatch.span() # location of match: (begin, end)
-            token = s[loc[0]:loc[1]] # string token
-            tokenList.append([loc, token])
-            s = s.replace(token, ' '*len(token), 1) # replace token with spaces
+    def split(self, string):
+        """Split string into valid string tokens"""
+        # Pipe Cleaner - fix problems arising from pipes in inner products
+        # This is only a problem with qm.py, need to move this to a preprocess
+        # method
+        innerProdMatches = re.finditer(r'<[a-zA-Z][a-zA-Z0-9]*[|][a-zA-Z][a-zA-Z0-9]*>', string)
+        for InnerProd in innerProdMatches:
+            subStr = InnerProd.group(0)
+            # double the pipe within the inner product
+            string = string.replace(subStr, subStr.replace('|', '||'))
+            
+        tokenList = []
+        for regex in self.inputDict:
+            tokenMatches = re.finditer(regex, string) # find all matches in string
+            for eachMatch in tokenMatches:
+                loc = eachMatch.span() # location of match: (begin, end)
+                token = string[loc[0]:loc[1]] # string token
+                tokenList.append([loc, token])
+                string = string.replace(token, ' '*len(token), 1) # replace token with spaces
 
-    # check to make sure all characters have been removed from s
-    leftover = s.strip()
-    if len(leftover) > 0:
-        loc = s.find(leftover[0])
-        raise InputError(leftover[0], loc)
+        # check to make sure all characters have been removed from s
+        leftover = string.strip()
+        if len(leftover) > 0:
+            loc = string.find(leftover[0])
+            raise InputError(leftover[0], loc)
 
-    tokenList.sort() # sort in place by location returned from span()
-    tokens = [item[1] for item in tokenList] # add only string tokens, not loc
-    return tokens
+        tokenList.sort() # sort in place by location returned from span()
+        tokens = [item[1] for item in tokenList] # add only string tokens, not loc
+        return tokens
 
-def assemble(self, tokenList):
-    """Assembles valid tokens into an output string."""
-    outputString = ''.join(tokenList) # combine tokens into string
-    outputString = outputString.replace('||', '|') # remove double pipes
-    return outputString
+    def assemble(self, tokenList):
+        """Assembles valid tokens into an output string."""
+        outputString = ''.join(tokenList) # combine tokens into string
+        outputString = outputString.replace('||', '|') # remove double pipes
+        return outputString
+
+
+class TreeBuilder(object):
+    """Builds Expression trees from an object list based on an order of
+operations list."""
+    def __init__(self, orderOfOperationsList):
+        self.order = orderOfOperationsList
+
+    def printTree(self, tree):
+        """Construct a string based on the expression tree"""
+        pass
+
+    def buildTree(self, tokenList):
+        """Construct a tree based on the string token list"""
+        pass
+
 
 # Define exceptions
 class ClassificationError(Exception):
