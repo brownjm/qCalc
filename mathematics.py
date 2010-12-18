1ec5724213170e70376c0e0c45ee6fa29fbc322c
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2010-12-17 17:20:57 -0700

    Moved Expression class from mathematics.py to io.py, since it is the node for a tree and not really a mathematical expression.

--- mathematics.py
+++ mathematics.py
@@ -9,6 +9,7 @@ class MathType(object):
         """Equivalence includes comparison of type and value"""
         return (type(self)==type(other)) and (self.val == other.val)
 
+
 class Number(MathType):
     """Wrapper class for numerical values"""
     def __init__(self, value):
@@ -19,32 +20,23 @@ class Number(MathType):
     def Value(self):
         return float(self.val)
 
+
 class Operation(MathType):
     """Class for basic mathematical operations, e.g. +, -, *, /"""
     def __init__(self, operation):
         MathType.__init__(self, operation)
+
 
 class Quantity(MathType):
     """Class to represent mathematical quantities, such as (A+3*B)"""
     def __init__(self, quantity):
         MathType.__init__(self, quantity)
 
+
 class Variable(MathType):
     """Class to represent a symbolic variable"""
     pass
 
-class Expression(object):
-    """Class to be used as a node when creating trees of Expressions"""
-    def __init__(self, left, operation=None, right=None):
-        self.left = left
-        self.op = operation
-        self.right = right
-
-    def __eq__(self, other):
-        """Equivalence includes comparison of children and operation"""
-        return (self.left == other.left) and \
-               (self.op == other.op) and \
-               (self.right == other.right)
 
 # Dictionary containing regular expressions of quantum mechanical string tokens
 # and their associated classes
