bd9364c38af99d4c4f3d5ccf425125dcb0eb6276
KT <kt@Ceres.(none)>
2011-01-22 15:40:14 -0700

    Created the plugin manager.

--- plugin.py
+++ plugin.py
@@ -1,1 +1,33 @@
-"""Initial file for plugin engine."""
+class PluginEngine(object):
+    def __init__(self):
+        self.loadedPlugins = {}
+
+        for lst in listsToBuild:
+            self.__dict__[lst] = []
+
+        for dct in dictsToBuild:
+            self.__dict__[dct] = {}
+            
+        for plugin in reqPlugins:
+            self.load(plugin)
+
+    def load(self, plugin):
+        print "Loading plugin:", plugin
+        self.loadedPlugins[plugin] = __import__(plugin)
+        
+        for lst in listsToBuild:
+            self.__dict__[lst].extend(self.loadedPlugins[plugin].__dict__[lst])
+
+        for dct in dictsToBuild:
+            self.__dict__[dct].update(self.loadedPlugins[plugin].__dict__[dct])
+    
+    def remove(self, plugin):
+        pass
+
+reqPlugins = ["mathematics"]
+addPlugins = []
+listsToBuild = ["containers", "parseOrder", "orderOfOperations"]
+dictsToBuild = ["inputDict", "outputDict"]
+
+if __name__ == '__main__':
+    pe = PluginEngine()
